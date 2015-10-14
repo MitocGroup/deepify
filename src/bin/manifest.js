7faa999fb6425a98ab75d77a79a03f131718f0f2
AlexanderC <alexander.moldova@gmail.com>
2015-10-14 15:30:03 +0300

    change commands order disaplayed in help

--- src/bin/manifest.js
+++ src/bin/manifest.js
@@ -14,6 +14,18 @@ module.exports = {
   description: 'DEEP CLI',
   commandsPath: './commands',
   commands: {
+    helloworld: {
+      example: 'deepify helloworld path/to/web_app',
+      description: 'Dump the "Hello World" sample web app',
+      opts: {
+      },
+      args: {
+        path: {
+          description: 'The path to dump the sample web app to',
+          required: true,
+        },
+      },
+    },
     server: {
       example: 'deepify server path/to/web_app -o',
       description: 'Run local development server',
@@ -125,14 +137,24 @@ module.exports = {
         },
       },
     },
-    helloworld: {
-      example: 'deepify helloworld path/to/web_app',
-      description: 'Dump the "Hello World" sample web app',
+    undeploy: {
+      example: 'deepify undeploy path/to/web_app',
+      description: 'Remove web app provisioning and uploaded data',
       opts: {
+        'cfg-bucket': {
+          alias: 'b',
+          description: 'AWS S3 system bucket name where the deploy config was persisted (ex. deep.prod.system.db0c09cc)',
+          required: false,
+        },
+        dirty: {
+          alias: 'd',
+          description: 'Force cleaning up all resources if .cfg.deeploy.json file missing',
+          required: false,
+        },
       },
       args: {
         path: {
-          description: 'The path to dump the sample web app to',
+          description: 'The path to the web app',
           required: true,
         },
       },
@@ -161,28 +183,6 @@ module.exports = {
         'dry-run': {
           alias: 'd',
           description: 'Work locally, without pushing microservices to the remote repository',
-          required: false,
-        },
-      },
-      args: {
-        path: {
-          description: 'The path to the web app',
-          required: true,
-        },
-      },
-    },
-    undeploy: {
-      example: 'deepify undeploy path/to/web_app',
-      description: 'Remove web app provisioning and uploaded data',
-      opts: {
-        'cfg-bucket': {
-          alias: 'b',
-          description: 'AWS S3 system bucket name where the deploy config was persisted (ex. deep.prod.system.db0c09cc)',
-          required: false,
-        },
-        dirty: {
-          alias: 'd',
-          description: 'Force cleaning up all resources if .cfg.deeploy.json file missing',
           required: false,
         },
       },
