9d155ca88842777b0d210990afda9a0ae7d77fb4
vcernomschi <vcernomschi@mitocgroup.com>
2016-01-13 17:39:46 +0200

    Change calling _mocha

--- src/bin/test.sh
+++ src/bin/test.sh
@@ -1,8 +1,8 @@
 if [ "$OSTYPE" != "msys" ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
-    babel-node `which isparta` cover --include 'lib/**/*.js' _mocha -- 'test/**/*.spec.js' --reporter spec --ui tdd --recursive
+    babel-node `which isparta` cover --include 'lib/**/*.js' `which _mocha` -- 'test/**/*.spec.js' --reporter spec --ui tdd --recursive
 elif [ "$OSTYPE" == "win32" ] || [ "$OSTYPE" == "win64" ]; then
     echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
 else
     echo "Running from git-bash without gathering coverage"
-    babel-node _mocha --ui tdd --recursive --reporter spec
+    babel-node `which _mocha` --ui tdd --recursive --reporter spec
 fi
