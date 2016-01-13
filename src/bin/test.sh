c2c8a2060ba76cb44afdb947b1a1d1e52bc5131a
vcernomschi <vcernomschi@mitocgroup.com>
2016-01-13 15:24:49 +0200

    Add x64

--- src/bin/test.sh
+++ src/bin/test.sh
@@ -1,6 +1,6 @@
-if [ "$OSTYPE" != "msys" ] && [ "$OSTYPE" != "win32" ]; then
+if [ "$OSTYPE" != "msys" ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
     babel-node `which isparta` cover --include 'lib/**/*.js' _mocha -- 'test/**/*.spec.js' --reporter spec --ui tdd --recursive
-elif [ "$OSTYPE" == "win32" ]; then
+elif [ "$OSTYPE" == "win32" ] && [ "$OSTYPE" == "win64" ]; then
     echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
 else
     echo "Running from git-bash without gathering coverage"
