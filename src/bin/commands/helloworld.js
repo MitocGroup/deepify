a62eab6233f21e5a5e65067c2d7c3d662f6c7fd1
AlexanderC <alexander.moldova@gmail.com>
2016-01-15 18:21:21 +0200

    Fetch search besides helloworld

--- src/bin/commands/helloworld.js
+++ src/bin/commands/helloworld.js
@@ -9,22 +9,34 @@ module.exports = function(dumpPath) {
 
   // @todo: put it anywhere in a config
   var helloWorldRepoUrl = 'https://github.com/MitocGroup/deep-microservices-helloworld.git';
+  var searchRepoUrl = 'https://github.com/MitocGroup/deep-microservices-search.git';
 
   var Exec = require('../../lib.compiled/Helpers/Exec').Exec;
   var Bin = require('../../lib.compiled/NodeJS/Bin').Bin;
 
-  var cmd = new Exec(
+  new Exec(
     Bin.node,
     this.scriptPath,
     'install',
     helloWorldRepoUrl,
     dumpPath
-  );
-
-  cmd.run(function(result) {
+  ).run(function(result) {
     if (result.failed) {
       console.error(result.error);
       this.exit(1);
     }
-  }, true);
+
+    new Exec(
+      Bin.node,
+      this.scriptPath,
+      'install',
+      searchRepoUrl,
+      dumpPath
+    ).run(function(result) {
+      if (result.failed) {
+        console.error(result.error);
+        this.exit(1);
+      }
+    }.bind(this), true);
+  }.bind(this), true);
 };
