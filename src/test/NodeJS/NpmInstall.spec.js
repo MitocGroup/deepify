4fda3fd573ca52abfa0cfa84528ca48eba5d9877
vcernomschi <vcernomschi@mitocgroup.com>
2016-02-01 12:07:03 +0200

    Add new unit test for npm install

--- src/test/NodeJS/NpmInstall.spec.js
+++ src/test/NodeJS/NpmInstall.spec.js
@@ -22,13 +22,61 @@ suite('NodeJS/NpmInstall', function() {
     chai.expect(npmInstall._mainCmd).to.includes('npm install');
   });
 
-  test('Check constructor sets valid value for _dirs', function () {
+  test('Check constructor sets valid value for _dirs', function() {
     chai.expect(npmInstall).to.be.an.instanceOf(NpmInstall);
     chai.expect(npmInstall.dirs).to.be.eql([]);
   });
 
-  test('Check constructor sets valid value for _execArgs', function () {
+  test('Check constructor sets valid value for _extraArgs', function() {
+    chai.expect(Array.isArray(npmInstall.extraArgs)).to.be.equal(true);
+    chai.expect(npmInstall.extraArgs).to.eql([]);
+  });
+
+  test('Check _execArgs getter', function() {
     chai.expect(Array.isArray(npmInstall._execArgs)).to.be.equal(true);
+    chai.expect(npmInstall._execArgs[0]).to.includes('npm install');
+  });
+
+  test('Check addExtraArg()', function() {
+    let length = npmInstall.extraArgs.length;
+    let newArg = 'logLevel=debug';
+
+    let actualResult = npmInstall.addExtraArg(newArg);
+
+    chai.expect(actualResult).to.be.an.instanceOf(NpmInstall);
+    chai.expect(actualResult.extraArgs.length).to.equal(++length);
+    chai.expect(actualResult.extraArgs.pop()).to.equal(newArg);
+  });
+
+  test('Check _chunkArray() returns valid array', function() {
+    let toChunkArray = ['one', 'two', 'three', 'four'];
+    let expectedResult = [['one', 'two', 'three'], ['four']];
+    let size = 3;
+
+    let actualResult = NpmInstall._chunkArray(toChunkArray, size);
+
+    chai.expect(actualResult).to.be.an.instanceOf(Array);
+    chai.expect(actualResult).to.eql(expectedResult);
+  });
+
+  test('Check _newInstance() returns new NpmInstall instance with dirs.length = 3', function() {
+    let args = ['mocha', '-g', 'logLevel=debug'];
+
+    let actualResult = npmInstall._newInstance(args);
+
+    chai.expect(actualResult).to.be.an.instanceOf(NpmInstall);
+    chai.expect(actualResult.extraArgs).to.eql([]);
+    chai.expect(actualResult.dirs).to.eql(args);
+  });
+
+  test('Check _newInstance() returns new NpmInstall instance with dirs.length = 3', function() {
+    let args = ['mocha'];
+
+    let actualResult = npmInstall._newInstance(args);
+
+    chai.expect(actualResult).to.be.an.instanceOf(NpmInstall);
+    chai.expect(actualResult.extraArgs).to.eql([]);
+    chai.expect(actualResult.dirs).to.eql(args);
   });
 
 });
