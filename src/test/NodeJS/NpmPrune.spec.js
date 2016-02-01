9b9fac5457a920a94f540fa17aa1316e40db8a44
vcernomschi <vcernomschi@mitocgroup.com>
2016-02-01 16:02:09 +0200

    Add new unit tests for NpmPrune

--- src/test/NodeJS/NpmPrune.spec.js
+++ src/test/NodeJS/NpmPrune.spec.js
@@ -2,9 +2,32 @@
 
 import chai from 'chai';
 import {NpmPrune} from '../../lib/NodeJS/NpmPrune';
+import {NpmInstall} from '../../lib/NodeJS/NpmInstall';
 
 suite('NodeJS/NpmPrune', function() {
+  let args = 'mocha -g';
+  let npmPrune = null;
+
   test('Class NpmPrune exists in NodeJS/NpmPrune', function() {
     chai.expect(typeof NpmPrune).to.equal('function');
+  });
+
+  test('Check constructor sets _cmd = null', function() {
+    npmPrune = new NpmPrune(args);
+    chai.expect(npmPrune).to.be.an.instanceOf(NpmPrune);
+  });
+
+  test('Check _newInstance() returns new NpmInstall instance with dirs.length = 3', function() {
+    let args = ['mocha', '-g', 'logLevel=debug'];
+
+    let actualResult = npmPrune._newInstance(args);
+
+    chai.expect(actualResult).to.be.an.instanceOf(NpmInstall);
+    chai.expect(actualResult.extraArgs).to.eql([]);
+    chai.expect(actualResult.dirs).to.eql(args);
+  });
+
+  test('Check _mainCmd getter returns valid string', function() {
+    chai.expect(npmPrune._mainCmd).to.includes('npm prune');
   });
 });
