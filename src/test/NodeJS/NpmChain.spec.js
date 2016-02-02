f5d892a473392c99826769170f8d0a178e306d2d
vcernomschi <vcernomschi@mitocgroup.com>
2016-02-02 10:40:51 +0200

    Adding new unit tests for NpmListDependencies

--- src/test/NodeJS/NpmChain.spec.js
+++ src/test/NodeJS/NpmChain.spec.js
@@ -23,14 +23,13 @@ suite('NodeJS/NpmChain', function() {
     npmChain = new NpmChain(npmLink);
     chai.expect(npmChain).to.be.an.instanceOf(NpmChain);
     chai.expect(npmChain.commands[0]).to.be.an.instanceOf(NpmInstall);
-    chai.expect(npmChain.commands).to.be.eql([npmLink]);
+    chai.expect(npmChain.commands).to.include(npmLink);
   });
 
-  //@todo - use other chai assert
   test('Check add() adds command', function() {
     npmChain.add(npmUpdate);
     chai.expect(npmChain).to.be.an.instanceOf(NpmChain);
-    chai.expect(npmChain.commands[npmChain.commands.length-1]).to.be.an.instanceOf(NpmInstall);
-    chai.expect(npmChain.commands).to.be.eql([npmLink, npmUpdate]);
+    chai.expect(npmChain.commands).to.include(npmLink);
+    chai.expect(npmChain.commands).to.include(npmUpdate);
   });
 });
--- src/test/NodeJS/NpmListDependencies.spec.js
+++ src/test/NodeJS/NpmListDependencies.spec.js
@@ -2,9 +2,40 @@
 
 import chai from 'chai';
 import {NpmListDependencies} from '../../lib/NodeJS/NpmListDependencies';
+import {NpmDependency} from '../../lib/NodeJS/NpmDependency';
+import {NpmDepsListException} from '../../lib/NodeJS/Exception/NpmDepsListException';
 
 suite('NodeJS/NpmListDependencies', function() {
+  let npmListDependencies = null
+
   test('Class NpmListDependencies exists in NodeJS/NpmListDependencies', function() {
     chai.expect(typeof NpmListDependencies).to.equal('function');
+  });
+
+  test('Check constructor sets _path', function() {
+    let currentDirectory = __dirname;
+    npmListDependencies = new NpmListDependencies(currentDirectory);
+    chai.expect(npmListDependencies).to.be.an.instanceOf(NpmListDependencies);
+    chai.expect(npmListDependencies.path).to.equal(currentDirectory);
+  });
+
+  test('Check list(0)', function() {
+    let actualResult = npmListDependencies.list(0);
+
+    chai.expect(actualResult).to.be.an.instanceOf(NpmDependency);
+  });
+
+  test('Check list() throws NpmDepsListException', function() {
+    let error = null;
+
+    npmListDependencies = new NpmListDependencies('invalidPath');
+
+    try {
+      npmListDependencies.list(0);
+    } catch (e) {
+      error = e;
+    }
+
+    chai.expect(error).to.be.an.instanceOf(NpmDepsListException);
   });
 });
