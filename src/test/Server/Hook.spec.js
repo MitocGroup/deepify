f25e7ee8e059c15f1277a2a576dd5a998fdc49a0
vcernomschi <vcernomschi@mitocgroup.com>
2016-02-03 12:43:59 +0200

    Add new unit tests for Hook

--- src/test/Server/Hook.spec.js
+++ src/test/Server/Hook.spec.js
@@ -2,9 +2,39 @@
 
 import chai from 'chai';
 import {Hook} from '../../lib/Server/Hook';
+import {Instance} from '../../lib/Server/Instance';
+import {Property_Instance as PropertyInstance} from 'deep-package-manager';
 
 suite('Server/Hook', () => {
+  let hook = null;
+  let server = null;
+  let propertyInstance = null;
+
   test('Class Hook exists in Server/Hook', () => {
     chai.expect(Hook).to.be.an('function');
+  });
+
+  test('Check constructor sets correctly properties for valid property', () => {
+    propertyInstance = new PropertyInstance('./test/TestMaterials/Property2', 'deeploy.test.json');
+
+    server = new Instance(propertyInstance);
+
+    hook = new Hook(propertyInstance);
+
+    chai.expect(hook).to.be.an.instanceOf(Instance);
+    chai.expect(hook.server).to.be.an.instanceOf(Instance);
+    chai.expect(hook.server).to.be.equal(server);
+  });
+
+  test('Check BEFORE static getter', () => {
+    chai.expect(Hook.BEFORE).to.be.equal('before');
+  });
+
+  test('Check AFTER static getter', () => {
+    chai.expect(Hook.AFTER).to.be.equal('after');
+  });
+
+  test('Check FILE_NAME static getter', () => {
+    chai.expect(Hook.FILE_NAME).to.be.equal('hook.server.js');
   });
 });
--- src/test/Server/Instance.spec.js
+++ src/test/Server/Instance.spec.js
@@ -25,12 +25,41 @@ suite('Server/Instance', () => {
     chai.expect(error).to.be.an.instanceOf(PropertyObjectRequiredException);
   });
 
-  test('Check constructor sets values for valid property', () => {
+  test('Check constructor sets correctly properties for valid property', () => {
     propertyInstance = new PropertyInstance('./test/TestMaterials/Property2', 'deeploy.test.json');
 
     server = new Instance(propertyInstance);
 
-    chai.expect(propertyInstance).to.be.an.instanceOf(PropertyInstance);
     chai.expect(server).to.be.an.instanceOf(Instance);
+    chai.expect(server.property).to.be.an.instanceOf(PropertyInstance);
+    chai.expect(server.property).to.be.equal(propertyInstance);
+    chai.expect(server.logger).to.be.an('function');
+    chai.expect(server.nativeServer).to.be.equal(null);
+    chai.expect(server.fs).to.be.equal(null);
+    chai.expect(server.host).to.be.equal(null);
+    chai.expect(server._localId).to.be.equal(0);
+  });
+
+  test('Check logger getter/setter', () => {
+    let logger = server.logger;
+    let newLogger = (...args) => {
+      console.debug(...args);
+    };
+
+    server.logger = newLogger;
+    chai.expect(server.logger).to.be.equal(newLogger);
+
+    server.logger = logger;
+    chai.expect(server.logger).to.be.equal(logger);
+  });
+
+  test('Check localId getter postincrements _localId', () => {
+    let localId = server._localId;
+    let expectedResult = localId + 1;
+
+    let actualResult = server.localId;
+
+    chai.expect(actualResult).to.be.equal(localId);
+    chai.expect(server._localId).to.be.equal(expectedResult);
   });
 });
