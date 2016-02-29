df947c5ee01ea6f03a8385ba30759269ce052673
AlexanderC <alexander.moldova@gmail.com>
2016-02-29 14:27:28 +0200

    Generate missing tests

---
+++ src/test/Registry/ConfigVars.spec.js
@@ -0,0 +1,13 @@
+// THIS TEST WAS GENERATED AUTOMATICALLY ON Mon Feb 29 2016 14:27:01 GMT+0200 (EET)
+
+'use strict';
+
+import chai from 'chai';
+import {ConfigVars} from '../../lib/Registry/ConfigVars';
+
+// @todo: Add more advanced tests
+suite('Registry/ConfigVars', function() {
+  test('Class ConfigVars exists in Registry/ConfigVars', () => {
+    chai.expect(ConfigVars).to.be.an('function');
+  });
+});
