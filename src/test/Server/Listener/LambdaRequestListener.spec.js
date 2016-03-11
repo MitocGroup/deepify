0179735a9071304711562d36404f99086efe94b8
dragosh1011 <dragos.palade94@gmail.com>
2016-03-11 12:09:03 +0200

    Update unit test

---
+++ src/test/Server/Listener/LambdaRequestListener.spec.js
@@ -0,0 +1,13 @@
+// THIS TEST WAS GENERATED AUTOMATICALLY ON Fri Mar 11 2016 11:45:32 GMT+0200 (EET)
+
+'use strict';
+
+import chai from 'chai';
+import {LambdaRequestListener} from '../../../lib/Server/Listener/LambdaRequestListener';
+
+// @todo: Add more advanced tests
+suite('Server/Listener/LambdaRequestListener', function() {
+  test('Class LambdaRequestListener exists in Server/Listener/LambdaRequestListener', () => {
+    chai.expect(LambdaRequestListener).to.be.an('function');
+  });
+});
