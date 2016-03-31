3f2670fd9113c9a8bf63078080048ee09b5ae3d7
mgoria <goria.marcel@gmail.com>
2016-04-01 01:07:23 +0300

    Assure deps are installed before instaling deepify

---
+++ src/node-bin/assure-deps.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+babel=$(which babel)
+
+if [ -z ${babel} ]; then
+	echo "Seems like babel is not installed! Installing babel v5 as default transpiler..."
+	echo ""
+    npm install babel@5.*.* -g
+
+    babel=$(which babel)
+    babel_version=$(babel --version)
+    echo "Installed babel ${babel_version}"
+fi
