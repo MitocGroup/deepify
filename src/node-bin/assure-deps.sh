32e5d9b89298639dac0de7f85b4034d54dd4678c
AlexanderC <alexander.moldova@gmail.com>
2016-04-01 11:31:58 +0300

    Replace tabs

--- src/node-bin/assure-deps.sh
+++ src/node-bin/assure-deps.sh
@@ -3,11 +3,12 @@
 babel=$(which babel)
 
 if [ -z ${babel} ]; then
-	echo "Seems like babel is not installed! Installing babel v5 as default transpiler..."
-	echo ""
-    npm install babel@5.*.* -g
+    echo "Seems like babel is not installed! Installing babel v5 as default transpiler..."
+    echo ""
+    npm install babel@5.x -g
 
     babel=$(which babel)
     babel_version=$(babel --version)
+    
     echo "Installed babel ${babel_version}"
 fi
