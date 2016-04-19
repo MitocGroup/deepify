f528256b0cb24b7d4faeeb5dfba2d25598cdb5a9
CCristi <ccovali@pycoding.biz>
2016-04-19 11:47:15 +0300

    # Fix travis

--- src/node-bin/test.sh
+++ src/node-bin/test.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 if [ "$OSTYPE" != "msys" ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
-    deepify compile-es6 test -x .js
-    deepify compile-es6 lib -x .js
-    isparta cover --include 'lib/**/*.js' `which _mocha` -- 'test/Helpers/Exec.spec.js' --reporter spec --ui tdd --recursive --timeout 20s
+    COMPILE_DIR='./compile';
+    deepify compile-es6 test -x .js --out-dir ${COMPILE_DIR}/test
+    deepify compile-es6 lib -x .js --out-dir ${COMPILE_DIR}/lib
+    isparta cover --include ${COMPILE_DIR}/lib/**/*.js `which _mocha` -- ${COMPILE_DIR}/test/**/*.spec.js --reporter spec --ui tdd --recursive --timeout 20s
 elif [ "$OSTYPE" == "win32" ] || [ "$OSTYPE" == "win64" ]; then
     echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
 else
