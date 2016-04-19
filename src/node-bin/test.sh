c12cfb2728cd8c5e33a1549bd9293971d128b41d
CCristi <ccovali@pycoding.biz>
2016-04-19 12:01:30 +0300

    # Fix travis

--- src/node-bin/test.sh
+++ src/node-bin/test.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 if [ "$OSTYPE" != "msys" ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
     COMPILE_DIR='./compile';
-    deepify compile-es6 test -x .js --out-dir ${COMPILE_DIR}/test
-    deepify compile-es6 lib -x .js --out-dir ${COMPILE_DIR}/lib
-    isparta cover --include ${COMPILE_DIR}/lib/**/*.js `which _mocha` -- ${COMPILE_DIR}/test/**/*.spec.js --reporter spec --ui tdd --recursive --timeout 20s
+
+    [ -d ${COMPILE_DIR} ] && rm -rf ${COMPILE_DIR};
+
+    DO_COMPILE() {
+        local resource=$1;
+
+        babel -x .js --presets es2015-node4 --plugins add-module-exports,transform-es2015-classes \
+            ${resource} --out-dir ${COMPILE_DIR}/${resource}
+    }
+
+    DO_COMPILE lib;
+    DO_COMPILE test;
+
+    isparta cover --include ${COMPILE_DIR}/lib/**/*.js `which _mocha` -- ${COMPILE_DIR}/test/**/*.spec.js \
+        --reporter spec --ui tdd --recursive --timeout 20s
 elif [ "$OSTYPE" == "win32" ] || [ "$OSTYPE" == "win64" ]; then
     echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
 else
