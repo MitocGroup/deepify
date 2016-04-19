ba0aff521fe67229ec7deca19905f5decfbf6b06
CCristi <ccovali@pycoding.biz>
2016-04-19 10:03:32 +0300

    travis fix

--- src/node-bin/test.sh
+++ src/node-bin/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 if [ "$OSTYPE" != "msys" ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
-    babel-node `which isparta` cover --include 'lib/**/*.js' `which _mocha` -- 'test/**/*.spec.js' --reporter spec --ui tdd --recursive --timeout 20s
+    babel-node --presets 'node_modules/babel-preset-es2015' --plugins 'node_modules/babel-plugin-transform-es2015-classes' \
+    `which isparta` cover --include 'lib/**/*.js' `which _mocha` -- 'test/**/*.spec.js' --reporter spec --ui tdd --recursive --timeout 20s
 elif [ "$OSTYPE" == "win32" ] || [ "$OSTYPE" == "win64" ]; then
     echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
 else
