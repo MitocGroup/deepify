8596ce77a486f81f4ec55ee4f406d1cfd7c76455
CCristi <ccovali@pycoding.biz>
2016-04-20 15:49:44 +0300

    # Assure babel preset are loaded from npm global

--- src/node-bin/assure-deps.sh
+++ src/node-bin/assure-deps.sh
@@ -6,13 +6,11 @@ if [ -z ${babel} ]; then
     echo "Seems like babel is not installed! Installing babel v5 as default transpiler..."
     echo ""
     npm install babel-cli@6.x -g
+    npm install babel-preset-es2015 -g
+    npm install babel-plugin-add-module-exports -g
 
     babel=$(which babel)
     babel_version=$(babel --version)
-    
+
     echo "Installed babel ${babel_version}"
 fi
-
-echo "Installing babel-es2015 preset"
-npm install babel-preset-es2015
-npm install babel-plugin-add-module-exports
