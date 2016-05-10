6ed36182e511f7af56545f1be11a778527c23b18
CCristi <covali.christian@gmail.com>
2016-05-10 18:32:38 +0300

    # bash fix

--- src/hooks/install_templates.sh
+++ src/hooks/install_templates.sh
@@ -5,7 +5,7 @@ INSTALL_PATH=${SCRIPT_PATH}/../resources/skeletons;
 
 [ -d ${INSTALL_PATH} ] && rm -rf ${INSTALL_PATH}
 
-echo "Installing DeepMicroservicesSkeleton in '${MS_PATH}'"
+echo "Installing DeepMicroservicesSkeleton in '${INSTALL_PATH}'"
 
 git clone --depth 1 https://github.com/MitocGroup/deep-microservices-skeleton ${INSTALL_PATH} && \
 ls -A ${INSTALL_PATH} | grep -v tpl | xargs -I '{}' rm -rf ${INSTALL_PATH}/{};
