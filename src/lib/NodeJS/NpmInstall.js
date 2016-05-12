a956f74e5de70554714d08395e9d2ef80126143b
vcernomschi <vcernomschi@mitocgroup.com>
2016-05-12 14:22:08 +0300

    Update chunk size calculation

--- src/lib/NodeJS/NpmInstall.js
+++ src/lib/NodeJS/NpmInstall.js
@@ -193,9 +193,11 @@ export class NpmInstall {
    * @returns {Number}
    */
   static get DEFAULT_CHUNK_SIZE() {
-    return Math.min(
-      Math.ceil(OS.totalmem() / 1024 / 1024 / 1024),
+    let calculatedChunkSize = Math.min(
+      Math.ceil(OS.freemem() * 2 / Math.pow(1024, 3)),
       OS.cpus().length
     );
+
+    return (calculatedChunkSize > 8) ? 8 : (calculatedChunkSize < 2) ? 2 : calculatedChunkSize;
   }
 }
