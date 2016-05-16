5d3412c5723798fb6f057c0b83d57dec397d5462
vcernomschi <vcernomschi@mitocgroup.com>
2016-05-16 12:24:19 +0300

    Fix coverage

--- test/bin/coverage.sh
+++ test/bin/coverage.sh
@@ -2,15 +2,23 @@
 
 source $(dirname $0)/_head.sh
 
+### Update paths ###
+
+SEARCH_VALUE="/compile"
+REPLACE_VALUE=""
+
+sed -e "s@${SEARCH_VALUE}@${REPLACE_VALUE}@g" ${__SRC_PATH}"/coverage/lcov.info" > ${__SRC_PATH}"/coverage/coverage.info"
+
 ### Upload Coverage info to Codacy ###
 
-cat ${__SRC_PATH}"/coverage/lcov.info" | codacy-coverage --debug
+cat ${__SRC_PATH}"/coverage/coverage.info" | codacy-coverage --debug
 
 ### Log top 20 file paths to be able see paths format from travis###
-head -n 20 ${__SRC_PATH}"/coverage/lcov.info"
+head -n 20 ${__SRC_PATH}"/coverage/coverage.info"
 
 ### Cleanup! ###
 
 __CMD='rm -rf ./coverage'
 
 subpath_run_cmd ${__SRC_PATH} "$__CMD"
+
