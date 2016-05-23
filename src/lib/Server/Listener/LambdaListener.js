7af08cf3b3414ed71f244a2d5afa3d3856ac6220
CCristi <covali.christian@gmail.com>
2016-05-23 12:32:30 +0300

    # Message change

--- src/lib/Server/Listener/LambdaListener.js
+++ src/lib/Server/Listener/LambdaListener.js
@@ -53,7 +53,7 @@ export class LambdaListener extends AbstractListener {
         try {
           lambdaConfig = FileSystemExtra.readJsonSync(lambdaConfigFile);
         } catch (e) {
-          return event.send500(`Missing or broken lambda _config.json in ${lambdaConfig}`);
+          return event.send500(`Missing or broken lambda _config.json in ${lambdaConfigFile}`);
         }
 
         this._runLambda(event, lambdaConfig, payload, isAsync);
