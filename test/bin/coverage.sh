22e09ab3dbaea0f185d3bf4a81826dcdf4640ed5
Vitali Cernomschi <vcernomschi@mitocgroup.com>
2016-05-24 18:19:15 +0300

    Improve execution time and update coverage report to es6 (#196)
    * Update path in coverage
    
    * Remove isparta from deps
    
    * Update coverage bash
    
    * Test with full paths
    
    * Send raw coverage
    
    * Regenerate codacy tokens
    
    * Use src/* path in coverage report
    
    * Remove unnecessary branches from travis
    
    * Update comments

--- test/bin/coverage.sh
+++ test/bin/coverage.sh
@@ -2,23 +2,28 @@
 
 source $(dirname $0)/_head.sh
 
-### Update paths ###
-
-SEARCH_VALUE=$(pwd -P)
+################################################################
+### Update paths to have src/* file in coverage report       ###
+### https://github.com/codacy/node-codacy-coverage/issues/26 ###
+################################################################
+SEARCH_VALUE=$(pwd -P)"/"
 REPLACE_VALUE=""
 
 sed -e "s@${SEARCH_VALUE}@${REPLACE_VALUE}@g" ${__SRC_PATH}"/coverage/lcov.info" > ${__SRC_PATH}"/coverage/coverage.info"
 
+######################################
 ### Upload Coverage info to Codacy ###
-
+######################################
 cat ${__SRC_PATH}"/coverage/coverage.info" | codacy-coverage --debug
 
-### Log top 20 file paths to be able see paths format from travis###
+#####################################################################
+### Log top 20 file paths to be able see paths format from travis ###
+#####################################################################
 head -n 20 ${__SRC_PATH}"/coverage/coverage.info"
 
-### Cleanup! ###
-
+#############################################
+### Cleanup! Remove all generated reports ###
+#############################################
 __CMD='rm -rf ./coverage'
 
 subpath_run_cmd ${__SRC_PATH} "$__CMD"
-
