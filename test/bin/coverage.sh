66cdddbf0806eba6b04fc7a29b4439c30608ba67
vcernomschi <vcernomschi@mitocgroup.com>
2016-05-24 17:12:18 +0300

    Send raw coverage

--- test/bin/coverage.sh
+++ test/bin/coverage.sh
@@ -7,20 +7,19 @@ source $(dirname $0)/_head.sh
 ####################
 #SEARCH_VALUE=$(pwd -P)"/"
 #REPLACE_VALUE=""
-SEARCH_VALUE=""
-REPLACE_VALUE=""
-
-sed -e "s@${SEARCH_VALUE}@${REPLACE_VALUE}@g" ${__SRC_PATH}"/coverage/lcov.info" > ${__SRC_PATH}"/coverage/coverage.info"
+#
+#sed -e "s@${SEARCH_VALUE}@${REPLACE_VALUE}@g" ${__SRC_PATH}"/coverage/lcov.info" > ${__SRC_PATH}"/coverage/coverage.info"
 
 ######################################
 ### Upload Coverage info to Codacy ###
 ######################################
-cat ${__SRC_PATH}"/coverage/coverage.info" | codacy-coverage --debug
+#cat ${__SRC_PATH}"/coverage/coverage.info" | codacy-coverage --debug
+cat ${__SRC_PATH}"/coverage/lcov.info" | codacy-coverage --debug
 
 #####################################################################
 ### Log top 20 file paths to be able see paths format from travis ###
 #####################################################################
-head -n 20 ${__SRC_PATH}"/coverage/coverage.info"
+head -n 20 ${__SRC_PATH}"/coverage/lcov.info"
 
 #############################################
 ### Cleanup! Remove all generated reports ###
