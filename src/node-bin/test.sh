d39d20d75a9e2b9ee35b552a76b91cf11e3a4e73
CCristi <covali.christian@gmail.com>
2016-05-30 17:43:11 +0300

    Fix travis CI

--- src/node-bin/test.sh
+++ src/node-bin/test.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 if [ "$OSTYPE" != "msys" ] && [ "$OSTYPE" != "win32" ] && [ "$OSTYPE" != "win64" ]; then
+    echo '{
+      "presets": [
+        "es2015"
+      ]
+    }' > .babelrc
 
     babel-node $(npm root -g)/istanbul/lib/cli.js cover `which _mocha` -- 'test/**/*.spec.js' \
     --reporter spec --ui tdd --recursive --timeout 20s
 
+    rm .babelrc
 elif [ "$OSTYPE" == "win32" ] || [ "$OSTYPE" == "win64" ]; then
     echo "You should have installed and configured http://git-scm.com/ and run all bash command by using git-bash.exe"
 else
